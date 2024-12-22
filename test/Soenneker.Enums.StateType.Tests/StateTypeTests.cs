76517e5c160d7cdba1554924eabd0ab0b607e978
Jake Soenneker <jake@soeneker.com>
2024-12-22 14:01:49 +0000

    Tests

--- test/Soenneker.Enums.StateType.Tests/StateTypeTests.cs
+++ test/Soenneker.Enums.StateType.Tests/StateTypeTests.cs
@@ -1,14 +1,17 @@
 using Soenneker.Tests.FixturedUnit;
 using Xunit;
 
-
 namespace Soenneker.Enums.StateType.Tests;
 
 [Collection("Collection")]
 public class StateTypeTests : FixturedUnitTest
 {
-
     public StateTypeTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    {
+    }
+
+    [Fact]
+    public void Default()
     {
 
     }
